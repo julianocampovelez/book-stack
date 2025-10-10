f498bedc10a60c07345cd256491b8524dc3c0a38
julianocampovelez <julianocampov@gmail.com>
2025-10-10 07:57:28 -0500

    Fix import path for failure.dart in BooksDatasource and BooksRepository

--- app/lib/features/home/domain/datasources/books_datasource.dart
+++ app/lib/features/home/domain/datasources/books_datasource.dart
@@ -1,5 +1,5 @@
 import 'package:app/core/entities/either_entity.dart';
-import 'package:app/core/network/error/failure.dart';
+import 'package:app/core/network/errors/failure.dart';
 import 'package:app/features/home/domain/entities/entities.dart';
 
 abstract class BooksDatasource {
