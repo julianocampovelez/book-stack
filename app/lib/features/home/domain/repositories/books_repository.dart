6b45ddaed63deb5a486c61a944cc6dbffe8cd1a1
julianocampovelez <julianocampov@gmail.com>
2025-10-10 07:09:21 -0500

    Add BooksRepository interface and export it in repositories file

---
+++ app/lib/features/home/domain/repositories/books_repository.dart
@@ -0,0 +1,7 @@
+import 'package:app/core/entities/either_entity.dart';
+import 'package:app/core/network/error/failure.dart';
+import 'package:app/features/home/domain/entities/entities.dart';
+
+abstract class BooksRepository {
+  Future<Either<Failure, List<Book>>> getBooks({int page = 1});
+}
---
+++ app/lib/features/home/domain/repositories/repositories.dart
@@ -0,0 +1,1 @@
+export 'package:app/features/home/domain/repositories/books_repository.dart';
