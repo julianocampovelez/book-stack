e20d3fbfe7d93e0cd2b76d97478747c591b0c5cb
julianocampovelez <julianocampov@gmail.com>
2025-10-10 12:38:32 -0500

    Refactor ItbookApiDatasource to improve error handling and remove unnecessary print statements; update getBookByIsbn13 method to return detailed server failure messages.

--- app/lib/features/home/infrastructure/datasources/itbook_api_datasource.dart
+++ app/lib/features/home/infrastructure/datasources/itbook_api_datasource.dart
@@ -1,4 +1,3 @@
-import 'package:app/core/network/dio_network.dart';
 import 'package:dio/dio.dart';
 
 import 'package:app/core/entities/either_entity.dart';
@@ -9,7 +8,7 @@ import 'package:app/core/utils/constants/network_paths.dart';
 import 'package:app/features/home/domain/datasources/datasources.dart';
 import 'package:app/features/home/domain/entities/book.dart';
 import 'package:app/features/home/infrastructure/mappers/book_mapper.dart';
-import 'package:app/features/home/infrastructure/models/itbook/itbook_response.dart';
+import 'package:app/features/home/infrastructure/models/models.dart';
 
 class ItbookApiDatasource extends BooksDatasource {
   final Dio _dio;
@@ -18,10 +17,7 @@ class ItbookApiDatasource extends BooksDatasource {
   @override
   Future<Either<Failure, List<Book>>> getBooks({int page = 1}) async {
     try {
-      print('Fetching books from Itbook API, page: $page');
-      print(_dio.options.baseUrl);
-      final Response response = await _dio.get(getBooksPath(page));
-      print('Response Status Code: ${response.statusCode}');
+      final Response response = await _dio.get(NetworkPaths.getBooksPath(page));
 
       if (response.statusCode != HttpStatusCode.ok) {
         return Left(
@@ -42,11 +38,45 @@ class ItbookApiDatasource extends BooksDatasource {
 
       return Right(books);
     } on DioException catch (e) {
-      print(e);
       final Failure failure = ErrorMapper.mapDioError(e);
       return Left(failure);
     } catch (e) {
-      print(e);
+      return Left(UnknownFailure(e.toString(), -1));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Book>> getBookByIsbn13(String isbn13) async {
+    try {
+      final Response response = await _dio.get(
+        NetworkPaths.getBookByIsbn13Path(isbn13),
+      );
+
+      print('Response data: ${response.data}');
+
+      print('Response status code: ${response.data.runtimeType}');
+
+      if (response.statusCode != HttpStatusCode.ok) {
+        return Left(
+          ServerFailure(
+            "Error del servidor: ${response.statusMessage}",
+            response.statusCode ?? -1,
+          ),
+        );
+      }
+
+      final ItbookDetailsResponse responseData = ItbookDetailsResponse.fromJson(
+        response.data,
+      );
+      print('Mapped response data: $responseData');
+
+      final Book book = BookMapper.itbookDetailsToEntity(responseData);
+
+      return Right(book);
+    } on DioException catch (e) {
+      final Failure failure = ErrorMapper.mapDioError(e);
+      return Left(failure);
+    } catch (e) {
       return Left(UnknownFailure(e.toString(), -1));
     }
   }
