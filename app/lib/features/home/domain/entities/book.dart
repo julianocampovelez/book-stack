feef235073c2ae457df5c916b706738b8655082f
julianocampovelez <julianocampov@gmail.com>
2025-10-10 07:08:28 -0500

    Add Book entity class with properties and constructor

---
+++ app/lib/features/home/domain/entities/book.dart
@@ -0,0 +1,35 @@
+class Book {
+  String title;
+  String subtitle;
+  String isbn13;
+  String image;
+  String url;
+  double price;
+  String? authors;
+  String? publisher;
+  String? language;
+  String? isbn10;
+  String? pages;
+  String? year;
+  String? rating;
+  String? desc;
+  Map<String, String>? pdf;
+
+  Book({
+    required this.title,
+    required this.subtitle,
+    required this.isbn13,
+    required this.image,
+    required this.url,
+    required this.price,
+    this.authors,
+    this.publisher,
+    this.language,
+    this.isbn10,
+    this.pages,
+    this.year,
+    this.rating,
+    this.desc,
+    this.pdf,
+  });
+}
